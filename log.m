de539a8bb25f66f097bb29b41bb662cf5495a6b2
Jeff Kaufman <jeff@adverplex.com>
2011-10-08 08:54:31 -0400

    send output to stdout, then have a bash script rotate the logs

--- log.m
+++ log.m
@@ -5,26 +5,10 @@
 #include "idler.h"
 
 int main(int argc, char *argv[]) {
-  NSFileManager *fileManager;
   NSRunningApplication *app;
   NSEnumerator *running;
 
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-  NSString *log_fname = [[NSString stringWithString:@"~/.prodlog"]
-			  stringByExpandingTildeInPath];
-  NSString *newline = [NSString stringWithString:@"\n"];
-  NSString *space = [NSString stringWithString:@" "];
-
-
-
-  fileManager = [NSFileManager defaultManager];
-  if (![fileManager fileExistsAtPath:log_fname]) {
-    [fileManager createFileAtPath:log_fname contents:nil attributes:nil];
-  }
-
-  NSFileHandle *output = [NSFileHandle fileHandleForWritingAtPath:log_fname];
-
 
   running = [[[NSWorkspace sharedWorkspace] runningApplications] objectEnumerator];
   while ((app = [running nextObject])) {
@@ -33,14 +17,11 @@ int main(int argc, char *argv[]) {
 				[[NSDate date] timeIntervalSince1970]];
       NSString *idle = [NSString stringWithFormat:@"%0.0f",
 				 system_idle_time()];
-      
-      [output seekToEndOfFile];
-      [output writeData:[now dataUsingEncoding:NSUTF8StringEncoding]];
-      [output writeData:[space dataUsingEncoding:NSUTF8StringEncoding]];
-      [output writeData:[idle dataUsingEncoding:NSUTF8StringEncoding]];
-      [output writeData:[space dataUsingEncoding:NSUTF8StringEncoding]];
-      [output writeData:[app.localizedName dataUsingEncoding:NSUTF8StringEncoding]];
-      [output writeData:[newline dataUsingEncoding:NSUTF8StringEncoding]];
+
+      printf("%s %s %s\n", 
+	     [now UTF8String], 
+	     [idle UTF8String], 
+	     [app.localizedName UTF8String]);
     }
   }
 
---
+++ prodlog_helper.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DIR=~/.prodlog
+LOCKDIR=~/.prodlog/lock
+PIDFILE=~/.prodlog/lock/pid
+PRODLOG=prodlog
+
+mkdir "$DIR" &> /dev/null # fine if it already exists
+
+# locking
+if ! mkdir "$LOCKDIR" &> /dev/null
+then # we don't have the lock
+
+    # check if the original process is still active
+    if kill -0 $(cat "$PIDFILE") &> /dev/null
+    then # process is still active
+	exit 0
+    else # process died
+
+	# remove the pid file and the lock
+	rm "$PIDFILE"
+	rmdir "$LOCKDIR"
+
+	# restart
+	exec "$0" "$@"
+
+	# this line is never reached
+	exit 1
+    fi
+fi
+
+echo "$$" > "$PIDFILE"
+
+function outfile {
+    echo "$DIR"/$(($(date "+%s")/10000)).log
+}
+
+
+while [ "$$" = $(cat $PIDFILE) ]
+do
+    CUR_FNAME="$(outfile)"
+    "$PRODLOG" >> "$CUR_FNAME"
+
+    # zip old logs if needed
+    for x in "$DIR"/*.log
+    do
+	if [ $(basename "$x") != $(basename "$CUR_FNAME") ]
+	then # old log file
+	    gzip "$x"
+	fi
+    done
+
+    sleep 1
+done
